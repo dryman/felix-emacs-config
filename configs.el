3cc86b7024e6054a24b1490149c2545230764f97
Felix Chern <fchern@supplyframe.com>
2013-05-20 09:40:17 -0700

    remove bookmarks. Use w3m to open links in org-mode by default

--- configs.el
+++ configs.el
@@ -4,6 +4,8 @@
 (show-paren-mode t)
 (setq-default indent-tabs-mode nil)
 
+(add-to-list 'auto-mode-alist '("\\.\\(markdown\\|mkdn\\)" . markdown-mode))
+
 (setq backup-directory-alist '(("." . "~/.backup_emacs"))
   backup-by-copying t    ; Don't delink hardlinks
   version-control t      ; Use version numbers on backups
@@ -17,3 +19,15 @@
  '(mode-line ((t (:foreground "#f9f9f9"
                   :background "#bdbdbd"
                   :box nil)))) )
+
+(defadvice org-open-at-point (around org-open-at-point-choose-browser activate)
+  (let ((browse-url-browser-function
+         (cond ((equal (ad-get-arg 0) '(4))
+                'browse-url-generic)
+               ((equal (ad-get-arg 0) '(16))
+                'choose-browser)
+               (t
+                (lambda (url &optional new)
+                  (w3m-browse-url url t)))
+               )))
+    ad-do-it))
