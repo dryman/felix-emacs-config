987b7ab3b7372e1a42e3ac860f03692f14e2fb83
Felix Chern <fchern@google.com>
2016-07-13 20:04:49 -0700

    MATH-1 MATH-2 MATH-3 MATH-4 MATH-5 MATH-6 all in one

--- utils.el
+++ utils.el
@@ -9,3 +9,10 @@
   (let ((face (or (get-char-property (point) 'read-face-name)
                   (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
+;; the source code of keyboard_leds can be found at
+;; http://osxbook.com/book/bonus/chapter10/kbdleds/
+;; Could enable by
+;; (setq ring-bell-function caps-bell)
+(defun caps-bell
+  (call-process-shell-command "keyboard_leds -c1; keyboard_leds -c0" nil 0 nil))
