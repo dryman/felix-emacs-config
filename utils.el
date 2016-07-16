cf1ed81ffcb236473798f35c751d2fee61d7237c
Felix Chern <idryman@gmail.com>
2016-07-15 18:43:50 -0700

    MATH-9 download packages without asking

--- utils.el
+++ utils.el
@@ -23,12 +23,8 @@
 Return a list of installed packages or nil for every skipped package."
   (mapcar
    (lambda (package)
-     ;; (package-installed-p 'evil)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (progn
-             (package-refresh-contents)
-             (package-install package))
-         package)))
+     (if (not (package-installed-p package))
+         (progn
+           (package-refresh-contents)
+           (package-install package))))
    packages))
